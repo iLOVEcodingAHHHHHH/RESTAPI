2673185ffa11355ec10887e7abd8a883cc1e75a1
iLOVEcodingAHHHHHH <bad.moogoat@gmail.com>
2025-06-12 17:42:58 -0400

    email capable update v0.5

--- resources/user.py
+++ resources/user.py
@@ -1,3 +1,4 @@
+import os
 from flask.views import MethodView
 from flask_smorest import Blueprint, abort
 from passlib.hash import pbkdf2_sha256
@@ -5,29 +6,53 @@ from flask_jwt_extended import create_access_token, create_refresh_token, get_jw
 
 from blocklist import BLOCKLIST
 
+from sqlalchemy import or_
 from db import db
 from models import UserModel
-from schemas import UserSchema
-
+from schemas import UserSchema, UserRegisterSchema
+import requests
 
 blp = Blueprint("Users", "users", description="Operations on users")
+
+
+def send_simple_message(to, subject, body):
+    domain = os.getenv("MAILGUN_DOMAIN")
+    return requests.post(
+  		f"https://api.mailgun.net/v3/{domain}/messages",
+  		auth=("api", os.getenv('MAILGUN_API_KEY', 'MAILGUN_API_KEY')),
+  		data={"from": f"Mailgun Sandbox <postmaster@{domain}>",
+			"to": [to],
+  			"subject": subject,
+  			"text": body})
 
 
 @blp.route("/register")
 class UserRegister(MethodView):
 
-    @blp.arguments(UserSchema)
+    @blp.arguments(UserRegisterSchema)
     def post(self, user_data):
 
-        if UserModel.query.filter(UserModel.username == user_data["username"]).first():
-            abort(409, message="A user with that username already exist.")
+        if UserModel.query.filter(
+            or_(
+                UserModel.username == user_data["username"],
+                UserModel.email == user_data['email']
+                )
+        ).first():
+            abort(409, message="A user with that username or email already exist.")
 
         user = UserModel(
             username = user_data["username"],
+            email = user_data["email"],
             password=pbkdf2_sha256.hash(user_data["password"])
         )
         db.session.add(user)
         db.session.commit()
+
+        send_simple_message(
+            to=user.email,
+            subject="Success",
+            body=f'Hi {user.username}! You Signed up test email from mailgun'
+        )
 
         return {"message": "User created successfully"}, 201
     
